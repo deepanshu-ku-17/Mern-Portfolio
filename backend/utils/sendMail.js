dd991a98b2b1490464245de372134d4ae4778e51
deepanshu kumar <113617532+deepanshu-ku-17@users.noreply.github.com>
2026-06-25 13:03:44 +0530

    fix email port for render

--- backend/utils/sendMail.js
+++ backend/utils/sendMail.js
@@ -4,7 +4,9 @@ dotenv.config();
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
